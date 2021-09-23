c7bbc3954c9f76199d27c8d4d01b637c25c1363c
Vishesh Shrivastava <vishesh24cs138@satiengg.in>
2021-09-23 06:31:10 +0530

    work left : store the array of strings in local storage

--- app.js
+++ app.js
@@ -1,22 +1,38 @@
 const inputTxt = document.querySelector(".input-txt");
+let moonsun = 1;
 inputTxt.value = "Create a new todo...";
 inputTxt.addEventListener("click", function () {
   inputTxt.value = "";
-  inputTxt.classList.add("input-txt-clr");
+  if (moonsun == 1) {
+    inputTxt.classList.add("input-txt-clr-l");
+  } else {
+    inputTxt.classList.add("input-txt-clr");
+  }
 });
 inputTxt.addEventListener("blur", function () {
   inputTxt.value = "Create a new todo...";
+  if (moonsun == 1) {
+    inputTxt.classList.remove("input-txt-clr-l");
+  } else {
+    inputTxt.classList.remove("input-txt-clr");
+  }
   inputTxt.classList.remove("input-txt-clr");
 });
 
 const addition = document.querySelector(".addition");
 const boxCount = document.querySelector(".box-count");
 let count = 0;
-
- //everytime i persskey this happens
+let i = 1;
+function lgtordrk() {
+  if (moonsun == 1) {
+    return "box-l";
+  }
+  return "";
+}
+//everytime i persskey this happens
 document.addEventListener("keydown", function (e) {
   if (e.key == "Enter" && inputTxt.value != "") {
-    addition.innerHTML += `<div class="box">
+    addition.innerHTML += `<div class="box ${lgtordrk()}" id="box-${i}" >
         <div class="img-holder"> 
           <img src="./images/icon-check.svg" alt="" class="check-img hidden">
         </div>
@@ -26,6 +42,7 @@ document.addEventListener("keydown", function (e) {
     count++;
     boxCount.textContent = `${count}`;
     inputTxt.value = "";
+    i++;
   }
   //these variables keep gets update every time a key is pressed
   const imgHolder = document.querySelectorAll(".img-holder");
@@ -112,4 +129,51 @@ document.addEventListener("keydown", function (e) {
       }
     }
   });
+});
+
+//sun and moon
+const sun = document.querySelector(".sun");
+const moon = document.querySelector(".moon");
+
+moon.addEventListener("click", function () {
+  moonsun = 0;
+  document.body.classList.remove("body-l");
+  document.querySelector(".bg").classList.remove("bg-l");
+  moon.classList.add("hidden");
+  sun.classList.remove("hidden");
+  document.querySelector(".write").classList.remove("write-l");
+  document.querySelector(".input-txt").classList.remove("input-txt-l");
+  document.querySelector(".input-txt").classList.remove("input-txt-clr-l");
+  document.querySelector(".main-box").classList.remove("main-box-l");
+  for (let i = 0; i < document.querySelectorAll(".box").length; i++) {
+    document.querySelectorAll(".box")[i].classList.remove("box-l");
+  }
+  console.log(document.querySelectorAll(".pointer"));
+  document.querySelector("footer").classList.remove("footer-l");
+  for (let i = 0; i < document.querySelectorAll(".pointer").length; i++) {
+    document.querySelectorAll(".pointer")[i].classList.remove("pointer-l");
+  }
+  document.querySelector(".saving-desc").classList.remove("saving-desc-l");
+  document.querySelector(".what-to-show").classList.remove("what-to-show-l");
+});
+
+sun.addEventListener("click", function () {
+  moonsun = 1;
+  document.body.classList.add("body-l");
+  document.querySelector(".bg").classList.add("bg-l");
+  moon.classList.remove("hidden");
+  sun.classList.add("hidden");
+  document.querySelector(".write").classList.add("write-l");
+  document.querySelector(".input-txt").classList.add("input-txt-l");
+  document.querySelector(".input-txt").classList.add("input-txt-clr-l");
+  document.querySelector(".main-box").classList.add("main-box-l");
+  for (let i = 0; i < document.querySelectorAll(".box").length; i++) {
+    document.querySelectorAll(".box")[i].classList.add("box-l");
+  }
+  document.querySelector("footer").classList.add("footer-l");
+  for (let i = 0; i < document.querySelectorAll(".pointer").length; i++) {
+    document.querySelectorAll(".pointer")[i].classList.add("pointer-l");
+  }
+  document.querySelector(".saving-desc").classList.add("saving-desc-l");
+  document.querySelector(".what-to-show").classList.add("what-to-show-l");
 });
